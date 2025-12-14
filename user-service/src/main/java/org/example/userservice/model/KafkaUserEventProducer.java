eb877426019a81331767aef03d3bda5ef88ffcdd
korzunEvgeniy <korzun-ferrero@ya.ru>
2025-12-14 13:55:55 +0300

    Add eureka-server and api-gateway

--- user-service/src/main/java/org/example/userservice/model/KafkaUserEventProducer.java
+++ user-service/src/main/java/org/example/userservice/model/KafkaUserEventProducer.java
@@ -1,9 +1,11 @@
 package org.example.userservice.model;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.retry.annotation.CircuitBreaker;
 import org.springframework.stereotype.Service;
 
 @Service
+@CircuitBreaker
 public class KafkaUserEventProducer {
 
     private final KafkaTemplate<String, UserEvent> kafkaTemplate;
