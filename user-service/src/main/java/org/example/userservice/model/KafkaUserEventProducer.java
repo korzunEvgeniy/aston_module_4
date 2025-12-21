4bacfd1118ea936fe61df7acbe7fa475ea9fd5f9
korzunEvgeniy <korzun-ferrero@ya.ru>
2025-12-21 12:05:50 +0300

    Add configuration-service, update docker-compose

--- user-service/src/main/java/org/example/userservice/model/KafkaUserEventProducer.java
+++ user-service/src/main/java/org/example/userservice/model/KafkaUserEventProducer.java
@@ -1,11 +1,12 @@
 package org.example.userservice.model;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.retry.annotation.CircuitBreaker;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
-@CircuitBreaker
 public class KafkaUserEventProducer {
 
     private final KafkaTemplate<String, UserEvent> kafkaTemplate;
@@ -15,8 +16,15 @@ public class KafkaUserEventProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
+    @CircuitBreaker
     public void sendUserEvent(String operation, String email) {
         UserEvent event = new UserEvent(operation, email);
         kafkaTemplate.send(TOPIC, event);
+    }
+
+    public void sendFallback(String operation, String email, Throwable throwable) {
+        log.warn("CircuitBreaker OPEN: Kafka unavailable. Op: {}, Email: {}, Error: {}",
+                operation, email, throwable.getMessage());
+        // Можно сохранить в DLQ или БД для retry
     }
 }
